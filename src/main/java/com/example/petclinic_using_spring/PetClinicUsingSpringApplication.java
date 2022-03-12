5a85d0860ec9f8b57d6b4f952fe2cc7d6963e91e
AKHILREDDY1636 <akhilreddy.donakonda@gmail.com>
2022-03-12 15:56:39 +0530

    initial Project

---
+++ src/main/java/com/example/petclinic_using_spring/PetClinicUsingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinic_using_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicUsingSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicUsingSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petclinic_using_spring/PetClinicUsingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinic_using_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicUsingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
