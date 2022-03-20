d8e794dbca36030863fcb6ee2b98d33a0838a910
AKHILREDDY1636 <akhilreddy.donakonda@gmail.com>
2022-03-20 12:14:21 +0530

    added POJO model, Closes #2

---
+++ src/main/java/com/example/petclinic_using_spring/models/Owner.java
@@ -0,0 +1,4 @@
+package com.example.petclinic_using_spring.models;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/example/petclinic_using_spring/models/Vet.java
@@ -0,0 +1,4 @@
+package com.example.petclinic_using_spring.models;
+
+public class Vet extends Person{
+}
